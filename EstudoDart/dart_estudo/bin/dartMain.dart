dc7b3f81b8ef1d58c415da2559da19add1ead7d8
Saitozin <afre.luz10@gmail.com>
2022-10-06 16:06:09 -0300

    aaaa

---
+++ EstudoDart/dart_estudo/bin/dartMain.dart
@@ -0,0 +1,5 @@
+import 'package:dart_estudo/dartCalculate.dart' as dart_estudo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_estudo.calculate()}!');
+}
---
+++ EstudoDart/dart_estudo/test/dart_estudo_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_estudo/dartCalculate.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
